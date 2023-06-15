b2fbb370b4473268e8d6b88d420a3842e115acb0
sacha-ocheret <114064920+sacha-ocheret@users.noreply.github.com>
2023-06-15 10:02:49 +0300

    App apload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1 = 0, n2 = 1;
+            int sum;
+            Console.WriteLine('Числа Фiбаначi');
+            Console.Write(n1+ " " + n2 + " ");
+            for (int i = 0; i <= 40; i++)
+            {
+                sum = n1 + n2;
+                n1 = n2;
+                n2 = sum;
+                Console.Write(sum + " ");
+            }
+        }
+    }
+}
